ebda79dbf83429eeb3f35bd41fb018a7dde0fdd2
Laura <laura.makila81@gmail.com>
2025-04-06 14:08:57 +0300

    melkein kunnossa

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
   navLinks.classList.toggle('active');
 }
+
+/*palvelu*/
+function toggleDescription(id) {
+  const element = document.getElementById(id);
+  if (element.classList.contains('open')) {
+    element.classList.remove('open'); // Suljetaan laatikko
+  } else {
+    element.classList.add('open'); // Avataan laatikko
+  }
+}
